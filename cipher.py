bd240ad6f22a289f4ab28b7e6d4862c2900e4b95
Amir Ince <amir.ince@bison.howard.edu>
2023-04-30 04:09:04 +0000

    Iterating through each letter in line, check if letter is in alphabet

--- cipher.py
+++ cipher.py
@@ -6,3 +6,7 @@ shft_amt = int(sys.argv[1]) # getting the shft_amt from cmd
 
 for line in message: # iterating through each line in message
     line=line.upper() # converting current line to upper case
+
+    for letter in line: # iterating through each letter in line
+        if letter.isalpha() # checking if letter is alphabet leter
+
