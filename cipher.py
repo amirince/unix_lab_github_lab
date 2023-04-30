240dcfe1448796f8f4e95b61b4d2f5ed6fa27003
Amir Ince <amir.ince@bison.howard.edu>
2023-04-30 04:26:37 +0000

    Fixed syntax error

--- cipher.py
+++ cipher.py
@@ -8,7 +8,7 @@ for line in message: # iterating through each line in message
     line=line.upper() # converting current line to upper case
 
     for letter in line: # iterating through each letter in line
-        if letter.isalpha() # checking if letter is alphabet leter
+        if letter.isalpha(): # checking if letter is alphabet leter
             letter_ascii = ord(letter) # converting letter to ascii
             new_letter = (letter_ascii + shft_amt - 65) % 26 + 65 # getting ascii off new letter
             new_message += chr(new_letter)
