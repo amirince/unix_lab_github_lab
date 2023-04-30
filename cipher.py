b8c071dd5905f0f720ee3bd9d0832b28c0a815de
Amir Ince <amir.ince@bison.howard.edu>
2023-04-30 04:22:08 +0000

    convert letter to ascii, shift letter, add letter ot new message

--- cipher.py
+++ cipher.py
@@ -9,4 +9,6 @@ for line in message: # iterating through each line in message
 
     for letter in line: # iterating through each letter in line
         if letter.isalpha() # checking if letter is alphabet leter
-
+            letter_ascii = ord(letter) # converting letter to ascii
+            new_letter = (letter_ascii + shft_amt - 65) % 26 + 65 # getting ascii off new letter
+            new_message += chr(new_letter)
