6da55b5b1c0387f7b20a8ba2c073dfddd57cc16a
Amir Ince <amir.ince@bison.howard.edu>
2023-04-30 04:06:44 +0000

    Iterate through each line in message

--- cipher.py
+++ cipher.py
@@ -4,3 +4,5 @@ new_message="" # string variable to hold encrypted string
 
 shft_amt = int(sys.argv[1]) # getting the shft_amt from cmd
 
+for line in message: # iterating through each line in message
+    line=line.upper() # converting current line to upper case
